93b60ae89fa7c5a32246a2b3a671125cafe05e55
Karl Zipser <karlzipser@berkeley.edu>
2024-06-15 11:34:42 -0700

    gacp

--- code/classifier.py
+++ code/classifier.py
@@ -38,7 +38,7 @@ for x in range(32):
     for y in range(32):
         images=1*images
         images[:,:,x,y]=0
-        outputs=net(images).cpu().numpy()
+        outputs=net(images).detach().cpu().numpy()
         #print(outputs.size())
         m[x,y]=outputs[0,:].sum(axis=1)
 
