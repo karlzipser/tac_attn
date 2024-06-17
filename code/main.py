06b6da0cc28d1bec4d3d0376582b3b2f4d304ca2
Karl Zipser <karlzipser@berkeley.edu>
2024-06-17 02:06:40 -0700

    gacp

--- code/main.py
+++ code/main.py
@@ -1,7 +1,10 @@
-# python3 utilz2/dev/project.py --src tac --tag with_net
+## 79 ########################################################################
+# python3 utilz2/dev/project.py --src tac_attn --tag something
+
 if __name__ == '__main__':
     import sys
     print("Argument List:", str(sys.argv))
-    from .classifier import *
+    from .attention_map import *
 
 #EOF
+## 79 ########################################################################
--- params/a.py
+++ params/a.py
@@ -1,3 +1,8 @@
+## 79 ########################################################################
+
+#run_path='project_tac/15Jun24_13h42m54s-jake_long_train'
+run_path='project_tac/14Jun24_23h26m41s-with_net'
 batch_size=1
-num_workers=4
-device='cuda:0'
+num_workers=1
+#EOF
+## 79 ########################################################################
