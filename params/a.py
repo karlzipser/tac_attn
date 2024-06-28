6f5882c4a0bbd1a773857a1f492ea39f8d8a2d67
Karl Zipser <karl.zipser.k@gmail.com>
2024-06-28 10:42:24 -0700

    gacp

--- params/a.py
+++ params/a.py
@@ -1,7 +1,7 @@
 ## 79 ########################################################################
 
 #run_path='project_tac/15Jun24_13h42m54s-jake_long_train'
-run_path='project_tac/18Jun24_14h09m36s'
+run_path='project_tac/21Jun24_23h10m13s'
 batch_size=1
 num_workers=4
 device='cuda:1'
